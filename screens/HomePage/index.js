664cf19031e3422f2bbc67875d9bdfdc29775e64
Your Name <you@example.com>
2022-08-07 12:01:03 +0530

    remove trash and code cleanup

--- screens/HomePage/index.js
+++ screens/HomePage/index.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
-import sliderImg from "../../assets/images/slider-dec.png"
+import sliderImg from "../../assets/images/landingImage.jpg"
 import FeaturedBlogs from "../BlogsPage";
-import CategoryPage from "../CategoryPage";
 import ScrollTop from "../../components/ScrollToTop";
 import {Fab} from "@mui/material";
 import {KeyboardArrowUp} from "@mui/icons-material";
@@ -25,16 +24,17 @@ class HomePage extends Component {
                                              data-wow-duration="1s" data-wow-delay="1s">
                                             <div className="row">
                                                 <div className="col-lg-12">
-                                                    <h6>Digital Media Agency</h6>
-                                                    <h2>We Boost Your Website Traffic</h2>
-                                                    <p>This template is brought to you by TemplateMo website. Feel free
-                                                        to use this for a commercial purpose. You are not allowed to
-                                                        redistribute the template ZIP file on any other template
-                                                        website. Thank you.</p>
+                                                    <h6>Get ready for update</h6>
+                                                    <h2>Stay updated with news and techology.</h2>
+                                                    <p>
+                                                        Welcome to the new world so here this is our site here we get
+                                                        updates of latest news and technology really excited to getting
+                                                        started.
+                                                    </p>
                                                 </div>
                                                 <div className="col-lg-12">
                                                     <div className="border-first-button scroll-to-section">
-                                                        <a href="#">Free Quote</a>
+                                                        <a href="#">Continue</a>
                                                     </div>
                                                 </div>
                                             </div>
